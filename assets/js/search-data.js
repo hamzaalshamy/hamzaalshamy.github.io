aca7b559d21579ede3bd4e744c88282832b99849
hamzaalshamy <123656672+hamzaalshamy@users.noreply.github.com>
2025-08-20 01:30:09 +0000

    Deploying to gh-pages from @ hamzaalshamy/hamzaalshamy.github.io@f4067d5f016fe07df50dd3939ecdb75827d45028 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
-          title: "CV",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "nav-teaching",
+  },{id: "nav-teaching",
           title: "teaching",
           description: "Materials for courses you taught. Replace this text with your description.",
           section: "Navigation",
